b2a7e25074dd76957bf8cdc41eeabd18eb0607e8
Anderson Beauvais <e1061615@cmaisonneuve.qc.ca>
2021-02-24 18:40:26 -0500

    Update CommandScreen.js
    Retourche image ajout logo sa compile

--- component/CommandScreen.js
+++ component/CommandScreen.js
@@ -1,22 +1,37 @@
 import React, { Component,useContext,useEffect,useState  } from 'react';
 import { Image } from 'react-native';
-import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body } from 'native-base';
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
 import { AuthContext } from '../navigate/AuthProvider';
 import axios from 'axios';
 import database from '@react-native-firebase/database'
 import { firebase } from '@react-native-firebase/auth';
+import { render } from 'react-dom';
+import { color } from 'react-native-reanimated';
 
- const CommandScreen=({navigate})=> {
-  const [dishes,setDishes] = useState({})
-  const {logout} =useContext(AuthContext) 
-  useEffect(() => {
-    const onValueChange = database()
-    .ref('Dish')
-    .on('value', snapshot => {
-      setDishes(snapshot.val());
-    });
+ class  CommandScreen extends React.Component{
+  static contextType = AuthContext;
+  constructor(props) {
+   
+    super(props);
+    this.state = {
+      password: 0,
+      dishes:[],
+      auth:false
+    };
+  }  
+  //const {logout} =useContext(AuthContext) 
+ componentDidMount(){
+  const onValueChange = database()
+  .ref('Dish')
+  .on('value', snapshot => {
+    this.setState({dishes:snapshot.val()});
   });
- const dishProduit = dishes.map(dish=>{return <Card style={{flex: 0}}>
+
+ }
+  render(){
+  const { navigation } = this.props;
+    const{register,login}=this.context
+ const dishProduit = this.state.dishes.map(dish=>{return <Card key={dish.id} style={{flex: 0}}>
     <CardItem key={dish.id}>
       <Left>
         <Thumbnail source={{uri: 'Image URL'}} />
@@ -36,15 +51,24 @@ import { firebase } from '@react-native-firebase/auth';
     </CardItem>
     <CardItem>
       <Left>
-        <Button transparent textStyle={{color: '#87838B'}}>
-          <Icon name="logo-github" />
-          <Text>1,926 stars</Text>
+        <Button transparent textStyle={{color: '#87838B',}} >
+          <Icon style={{color:'#ffc107'}} name="star" />
+          <Text style={{fontSize:19}} >4.6</Text>
+       
         </Button>
       </Left>
+      <Right>
+      <Button full transparent textStyle={{color: '#87838B',}} >
+          <Text style={{fontSize:19}}>{dish.prix} $</Text>
+          <Text style={{fontSize:19,marginRight:11}}>Add to Cart</Text>
+        </Button>
+        
+      </Right>
     </CardItem>
   </Card>
 
   })
+  
     return (
         <Container>
         
@@ -53,7 +77,9 @@ import { firebase } from '@react-native-firebase/auth';
         </Content>
  
       </Container>
-    );
+    );}
+    
   }
+
   export default CommandScreen;
 
