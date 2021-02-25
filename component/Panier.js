7e2040d1b8961d1b3f1d80622ab943c6b78c01ae
Anderson Beauvais <e1061615@cmaisonneuve.qc.ca>
2021-02-25 00:10:03 -0500

    update desigh
    Prix Et taxe ajouté

--- component/Panier.js
+++ component/Panier.js
@@ -1,68 +1,71 @@
-import React,{useContext} from 'react'
-import { View ,Image} from 'react-native'
+import React, { useContext } from 'react'
+import { View, Image } from 'react-native'
 import { AuthContext } from '../navigate/AuthProvider';
 import { Container, Header, Content, Card, CardItem, Body, Text, Button } from 'native-base';
 export default class Panier extends React.Component {
-        static contextType = AuthContext;
-        constructor(props) {
-   
-            super(props);
-            this.state = {
-            total:0,
-            taxe:0.00
-            };
-          }  
+    static contextType = AuthContext;
+    constructor(props) {
+
+        super(props);
+        this.state = {
+            total: 0,
+            taxe: 0.00
+        };
+    }
 
 
-        componentDidMount(){
+    componentDidMount() {
 
 
-        }
-    render(){
+    }
+    render() {
         const { navigation } = this.props;
-        const{panier,setPanier}=this.context
+        const { panier, setPanier } = this.context
 
-        
-        const itemsCart = panier.map(item=>{
-            return   <Card key={item.id}>
-            <CardItem >
-              <Body style={{flex:1,flexDirection:'row'}}>
-               <Image style={{height:50,width:50}} source={{uri: item.url}} ></Image>
-                <Text>
-                   {item.nom}
-                </Text>
-                <Text>
-                   prix : {item.prix}
-                </Text>
-              </Body>
-            </CardItem>
-          </Card>
+
+        const itemsCart = panier.map(item => {
+            return <Card key={item.id}>
+                <CardItem >
+                    <Body style={{ flex: 1, flexDirection: 'row' }}>
+                        <Image style={{ height: 50, width: 50 }} source={{ uri: item.url }} ></Image>
+                        <Text style={{marginLeft:15}}>
+                            {item.nom}
+                        </Text>
+                        <Text>
+                            prix : {item.prix} $
+                        </Text>
+                    </Body>
+                </CardItem>
+            </Card>
         })
-       const sum = panier.reduce(function (total, currentValue) {
+        const sum = panier.reduce(function (total, currentValue) {
             return total + currentValue.prix;
         }, 0);
-    return (
-        <Container>
-        <Header />
-        <Content>
-            { !panier?
-            <Text style={{margin:0,textAlign:'center',fontSize:30}}>
-                Panier vide
-            </Text>:itemsCart}
-       
-        </Content>
-        <Card >
-            <CardItem >
-              <Body style={{flex:1,flexDirection:'row'}}>
-               
-                <Text>
-               Total  {sum} $
-                </Text>
-               
-                <Button rounded  ><Text>Pay</Text></Button>
-              </Body>
-            </CardItem>
-          </Card>
-      </Container>
-    )}
+        return (
+            <Container>
+                <Header />
+                <Content>
+                    {!panier ?
+                        <Text style={{ margin: 0, textAlign: 'center', fontSize: 30 }}>
+                            Panier vide
+            </Text> : itemsCart}
+
+                </Content>
+                <Card >
+                    <CardItem >
+                        <Body style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+
+                            <Text>
+                                Total:  {sum} $
+                            </Text>
+                            <Text>
+                              Taxe:  { parseFloat(sum * 0.1495).toFixed(2) } $
+                            </Text>
+                            <Button rounded  ><Text>Pay</Text></Button>
+                        </Body>
+                    </CardItem>
+                </Card>
+            </Container>
+        )
+    }
 } 
