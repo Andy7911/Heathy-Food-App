e7fa07246340482c143455e058ac7c6d7e2c6f34
Anderson Beauvais <e1061615@cmaisonneuve.qc.ca>
2021-02-26 23:23:04 -0500

    amelioration stripe icon

--- component/Formulaire.js
+++ component/Formulaire.js
@@ -25,7 +25,7 @@ export default class Formulaire extends Component {
           <View style={styles.centeredView}>
             <View style={styles.modalView}>
                   <View>
-                    <Image source={require('../image/stripe-payment-icon.png')} style={{width:200,height:100}} ></Image>
+                    <Image source={require('../image/PngItemStripe.png')} style={{width:230,height:75, marginBottom:10}} ></Image>
                   </View>
            <TextInput placeholder='Credit card' autoCompleteType='cc-number' keyboardType='numeric' autoCorrect style={{borderWidth:0.5,borderColor:'black',width:'100%',borderRadius:10}}></TextInput>
            <TextInput placeholder='Credit card 2' style={{borderWidth:0.5,borderColor:'black',width:'100%',borderRadius:10,marginTop:10,marginBottom:10}}></TextInput>
