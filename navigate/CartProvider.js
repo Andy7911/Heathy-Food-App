e89665ea16624360dc0ce763ebcaae37fcb94e85
Anderson Beauvais <e1061615@cmaisonneuve.qc.ca>
2021-03-20 20:06:39 -0400

    debut solution
    udate quantité

--- navigate/CartProvider.js
+++ navigate/CartProvider.js
@@ -1,4 +1,4 @@
-import React,{ useState }  from 'react'
+import React, { useState } from 'react'
 
 
 export const CartContext = React.createContext();
@@ -6,18 +6,38 @@ export const CartContext = React.createContext();
 export const CartProvider = ({ children }) => {
 
     const [panier, setPanier] = useState([]);
-    const [profil,setProfil] =useState([]);
+    const [profil, setProfil] = useState([]);
     return (<CartContext.Provider value={{
         panier,
         setPanier,
 
-        handleDelete(id){
+        handleDelete(id) {
             const items = panier.slice();
-            const index = items.findIndex(function(item){
-                return item.id ==id;
+            const index = items.findIndex(function (item) {
+                return item.id == id;
             })
-            items.splice(index,1)
+            items.splice(index, 1)
             setPanier(items);
+        },
+        addquatite(id, Dish) {
+            debugger;
+            const items = panier.slice();
+            const index = items.findIndex(function (item) {
+                return item.id == id;
+            })
+            if (index != -1) {
+                panier[index].quatité++;
+                const nbquatité = panier[index].quatité;
+                const nvprix = nbquatité * panier[index].prixIni;
+                panier[index].prix = nvprix;
+
+            }
+            else {
+                Dish.quatité = 1
+                Dish.prixIni = Dish.prix;
+                setPanier([...panier, Dish]);
+            }
+
         }
 
 
