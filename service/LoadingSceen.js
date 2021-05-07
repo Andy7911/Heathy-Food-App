873cb673f93f9434b1ce25cce5a394dc19015854
Anderson Beauvais <e1061615@cmaisonneuve.qc.ca>
2021-05-07 00:52:52 -0400

    Update LoadingSceen.js

--- service/LoadingSceen.js
+++ service/LoadingSceen.js
@@ -3,16 +3,16 @@ import React from 'react'
 
 export default function LoadingSceen() {
     return (
-        <View style={{flex:3 ,backgroundColor:'white'}}>
-        <View style={{flex:2,flexDirection:'row'}}>
-            <Image style={{width:'100%',height:'95%'}} 
+        <View style={{flex:3 ,backgroundColor:'white',flexDirection:'column',justifyContent:'center',alignContent:'center',alignItems:'center'}}>
+       
+            <Image style={{width:'100%',height:'95%',flex:2}} 
             source={require('../image/burger.gif')}>
 
             </Image>
-            </View>
-            <View style={{flex:1,flexDirection:'row',justifyContent:'center'}}>
-            <Text style={{fontSize:24}} >Merci de patienté ...</Text>
-            </View>
+            
+            
+            <Text style={{fontSize:24,flex:1}} >Merci de patienté ...</Text>
+            
  
         </View>
     )
