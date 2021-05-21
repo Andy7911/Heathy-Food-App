ce664edaced20693c4a4c05f22947978ec488d50
Anderson Beauvais <e1061615@cmaisonneuve.qc.ca>
2021-05-20 23:05:01 -0400

    Update UI Paimen

--- component/PaiementScreen.js
+++ component/PaiementScreen.js
@@ -1,57 +1,83 @@
-import React,{useState} from 'react'
-import { View,  } from 'react-native'
-import { Input, Container, Content, Header, Item, Icon,Text } from 'native-base';
+import React, { useState,useContext } from 'react'
+
+import { Input, Container, Content, Header, Item, Icon, Text,Button,Card,CardItem,Body,View } from 'native-base';
 import CreditCardDisplay from 'react-native-credit-card-display';
-
-
+import { CartContext } from '../navigate/CartProvider';
+ 
 
 
 
 export default function PaiementScreen() {
-    const [cardNumber,setCardNumber]= useState('4550');
+    const { panier } = useContext(CartContext);
+    const [cardNumber, setCardNumber] = useState('4550');
     const [cvc, setCvc] = useState('123');
     const [exp, setexp] = useState('04/02');
     let emailRegex = /\b(?:\d{4}[ -]?){3}(?=\d{4}\b)/;
 
-
+    const sum = panier.reduce(function (total, currentValue) {
+        return total + currentValue.sommePrix;
+    }, 0);
+    
     return (
 
         <Container >
-           
-            <Content  contentContainerStyle={{flexDirection:'column'}}>
-                <View style={{alignItems:'center'}}>
-                <CreditCardDisplay
-                    number={cardNumber}
-                    cvc={123}
-                    expiration={exp}
-                    name="John J. Doe"
-                    since="2004"
-                />
+
+            <Content contentContainerStyle={{ flexDirection: 'column' }}>
+                <View style={{ alignItems: 'center'}}>
+                    <CreditCardDisplay style={{height:50,with:100}}
+                        number={cardNumber}
+                        cvc={cvc}
+                        expiration={exp}
+                        name="John J. Doe"
+                        since="2004"
+                    />
                 </View>
-                <View style={{flex:1,marginTop:50,}}>
-                    <Text style={{fontSize:20}}>Numero de carte</Text>
-                <Item error={true}>
-                    
-                    <Input placeholder='Credit Card'  onChangeText={e=>setCardNumber(e)} />
-                    <Icon name='checkmark-circle' />
-                </Item>
+                <View style={{ flex: 1, marginTop: 10, }}>
+                    <Text style={{ fontSize: 20 }}>Numero de carte</Text>
+                    <Item error={true} style={{ width: '90%', height: 50 }}>
+
+                        <Input  placeholder='Credit Card' onChangeText={e => setCardNumber(e)} />
+                        <Icon name='checkmark-circle' />
+                    </Item>
                 </View>
-                <View style={{ flex:1,flexDirection:'row',alignItems:'center', justifyContent:'space-around'}}>
-                <Text style={{fontSize:20}}></Text>
-                <View style={{flexDirection:'column',width:'50%',height:'100%'}}>
-                <Text>cvc</Text>
-                <Item regular style={{marginTop:15,width:'40%',height:50}}>
-                    <Input style={{width:'50%',height:50}}  placeholder='Textbox with Success Input'  />
-                    <Icon name='checkmark-circle' />
-                </Item>
+                <View style={{ flex: 1, marginTop: 10, }}>
+                    <Text style={{ fontSize: 20 }}>Prenom et Nom</Text>
+                    <Item error={true} style={{ width: '90%', height: 50 }}>
+
+                        <Input  placeholder='Credit Card' onChangeText={e => setCardNumber(e)} />
+                        <Icon name='checkmark-circle' />
+                    </Item>
                 </View>
-                <View style={{flexDirection:'column',width:'50%',height:'100%'}}>
-                    <Text>cvc</Text>
-                <Item regular style={{marginTop:15,width:'40%',height:50}}>
-                    <Input  placeholder='Textbox with Success Input'  />
-                    <Icon name='checkmark-circle' />
-                </Item>
+                <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-around', marginTop: 15 }}>
+                    <Text style={{ fontSize: 20 }}></Text>
+                    <View style={{ flexDirection: 'column', width: '50%', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+                        <Text style={{ fontSize: 20 }} >Date expiration</Text>
+                        <Item regular style={{ marginTop: 15, width: '90%', height: 50 }}>
+                            <Input dataDetectorTypes='calendarEvent' style={{ width: '50%', height: 50 }} placeholder='Textbox with Success Input' onChangeText={e => setexp(e)} />
+                            <Icon name='checkmark-circle' />
+                        </Item>
+                    </View>
+                    <View style={{ flexDirection: 'column', width: '50%', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+                        <Text style={{ fontSize: 20 }}>Code de sécurité</Text>
+                        <Item regular style={{ marginTop: 15, width: '90%', height: 50 }}>
+                            <Input placeholder='Textbox with Success Input' onChangeText={e => setCvc(e)} />
+                            <Icon name='checkmark-circle' />
+                        </Item>
+                    </View>
                 </View>
+                <View style={{flexDirection:'column' ,alignItems:'center',justifyContent:'center'}}>
+                <Card>
+            <CardItem>
+              
+                <Text>
+                   Total:{sum}
+                </Text>
+            
+            </CardItem>
+          </Card>
+                    <Button full rounded info>
+                        <Text>Paiement</Text>
+                    </Button>
                 </View>
             </Content>
         </Container>
